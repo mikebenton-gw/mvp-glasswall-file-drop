d414e1aeb8a848f64be1283f53dc2414294716f6
GW\mdignum <mdignum@glasswallsolutions.com>
2019-11-18 14:15:08 +0000

    Call Api in handleDrop event. Parse json

--- src/CallApi.js
+++ src/CallApi.js
@@ -3,23 +3,43 @@ import DragAndDrop from './DragAndDrop'
 
 class CallApi extends Component {
 state = {
-    files: []
+    file: null,
+    isLoaded: false,
+    items: null
   }
- handleDrop = (files) => {
-    let fileList = this.state.files
-    for (var i = 0; i < files.length; i++) {
-      if (!files[i].name) return
-      fileList.push(files[i].name)
-    }
-    this.setState({files: fileList});
+ handleDrop = (file) => {
+   var data = new FormData();
+   data.append('file', file[0]);
+
+   var options = {
+     method: 'POST',
+     body: data
+   };
+
+   fetch("https://localhost:44319/api/values", options)
+   .then((res) => res.json())
+   .then((result) => {
+       this.setState({
+         isLoaded: true,
+         items: result
+       });
+     },
+     // Note: it's important to handle errors here
+     // instead of a catch() block so that we don't swallow
+     // exceptions from actual bugs in components.
+     (error) => {
+       this.setState({
+         isLoaded: true,
+         error
+       });
+     }
+   )
   }
 render() {
     return (
       <DragAndDrop handleDrop={this.handleDrop}>
         <div style={{height: 300, width: 250}}>
-          {this.state.files.map((file) =>
             <div><p>Calling Api</p></div>
-          )}
         </div>
       </DragAndDrop>
     )
