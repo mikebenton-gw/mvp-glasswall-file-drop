d404b8bc1b2cbe71c41c157f192fb024bb426448
Matt Dignum <matt.dignum@googlemail.com>
2020-03-18 16:28:40 +0000

    Call Analyse and Rebuild api's

--- src/api/engineApi.js
+++ src/api/engineApi.js
@@ -1,6 +1,7 @@
-const urlPrefix = 'https://glasswall-file-drop-api.azurewebsites.net'
-const analysisSuffix = '/api/sas/FileAnalysis';
-const protectSuffix = '/api/sas/FileProtect';
+const urlPrefix = ' https://24dyhnzh5h.execute-api.eu-west-2.amazonaws.com'
+const analysisSuffix = '/Beta/az/fileanalysis/analyse';
+const protectSuffix = '/Beta/az/filerebuild/rebuild';
+const apiKey = 'fw9vUniTYY3bEnO7xYPqU5sYnP8iYYyA4V9epdu8'
 
 const analyseFile = (file) => {
     var data = new FormData();
@@ -22,11 +23,14 @@ const callFileAnalysis = (url, data) => {
   const promise = new Promise((resolve, reject) => {
       resolve(fetch(url, {
         method: 'POST',
-        body: data
+        body: data,
+        headers: {
+          "x-api-key" : apiKey
+        } 
       })
       .then ((response) => {
         if (response.ok) {
-          return response.json()
+          return response.text()
         }
         else{
           throw new Error('Something went wrong');
@@ -41,7 +45,10 @@ const callFileProtect = (url, data) => {
   const promise = new Promise((resolve, reject) => {
       resolve(fetch(url, {
         method: 'POST',
-        body: data
+        body: data,
+        headers: {
+          "x-api-key" : apiKey
+        }
       })
       .then ((response) => {
         if (response.ok) {
